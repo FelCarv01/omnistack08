5691f0ea406ac85903d5c9e960da9b8ae9569b89
Felipe Carvalho <feliped3carvalho@gmail.com>
2019-08-07 23:22:13 -0300

    iniciado o backend com a rota devs api do github

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const server = express();
+
+mongoose.connect('mongodb+srv://Omni08:ePMQQ2acpOSM1dee@cluster0-r6lya.mongodb.net/omnistack?retryWrites=true&w=majority',{
+  useNewUrlParser: true
+});
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
