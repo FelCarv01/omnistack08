b559505ba0e563fd6bff138aa4b2cd42a9f25dd7
Felipe Carvalho <feliped3carvalho@gmail.com>
2019-08-08 00:50:12 -0300

    backend finalizado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const DevController = require("./controller/DevController");
+const LikeController = require("./controller/LikeController");
+const DisLikeController = require("./controller/DislikeController");
 
 const routes = express.Router();
 
+routes.get('/devs', DevController.index)
 routes.post("/devs", DevController.store);
+routes.post("/devs/:devId/likes", LikeController.store);
+routes.post("/devs/:devId/dislikes", DisLikeController.store);
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const routes = require("./routes");
 
 const server = express();
 
-mongoose.connect('mongodb+srv://Omni08:ePMQQ2acpOSM1dee@cluster0-r6lya.mongodb.net/omnistack?retryWrites=true&w=majority',{
-  useNewUrlParser: true
-});
+mongoose.connect(
+  "mongodb+srv://Omni08:ePMQQ2acpOSM1dee@cluster0-r6lya.mongodb.net/omnistack?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
